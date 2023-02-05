0c34954b6ecf1699846966e13001ebf71c841220
Derek Huang <djh458@stern.nyu.edu>
2023-02-05 17:46:46 -0500

    add 1.7.c and CMake target
    src/1.7.c
        Add program printing value of EOF as integer
    
    src/CMakeLists.txt
        Add target for 1.7

---
+++ src/1.7.c
@@ -0,0 +1,22 @@
+/**
+ * @file 1.7.c
+ * @author Derek Huang
+ * @brief The C Programming Language exercise 1-7
+ * @copyright MIT License
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PDCPL_HAS_PROGRAM_USAGE
+#include "pdcpl/cliopts.h"
+#include "pdcpl/core.h"
+
+PDCPL_PROGRAM_USAGE_DEF("Prints the value of EOF as an integer.")
+
+PDCPL_ARG_MAIN
+{
+  PDCPL_HANDLE_INFO_OPTS();
+  printf("EOF = %d\n", EOF);
+  return EXIT_SUCCESS;
+}
