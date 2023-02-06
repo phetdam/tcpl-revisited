138e177811fc61047113fe792ec30e9ff9613b61
Derek Huang <djh458@stern.nyu.edu>
2023-02-05 23:35:12 -0500

    update 1.6.c to actually use stdin + add string.h
    include/pdcpl/string.h
        Add string utilities header
    
    src/1.6.c
        Give up on opening memory buffer as FILE *, instead just reading
        from stdin with getchar() and update description. Remove errno
        handling macros, Windows code, and add conditional for handling
        of escape characters when printing

---
+++ include/pdcpl/string.h
@@ -0,0 +1,52 @@
+/**
+ * @file string.h
+ * @author Derek Huang
+ * @brief C/C++ header for string helpers
+ * @copyright MIT License
+ */
+
+#ifndef PDCPL_STRING_H_
+#define PDCPL_STRING_H_
+
+#include <stdio.h>
+
+/**
+ * Return the string associated with the escaped character.
+ *
+ * If the incoming character is not an escape character, return `NULL`. `EOF`
+ * is also handled here and treated as an escape character.
+ *
+ * @param c Incoming character
+ */
+static inline const char *
+pdcpl_stresc(int c)
+{
+  switch (c) {
+    case '\a':
+      return "\\a";
+    case '\b':
+      return "\\b";
+// on Windows, \e is not recognized as an escape character
+#ifndef _WIN32
+    case '\e':
+      return "\\e";
+#endif  // _WIN32
+    case '\f':
+      return "\\f";
+    case '\n':
+      return "\\n";
+    case '\r':
+      return "\\r";
+    case '\t':
+      return "\\t";
+    case '\v':
+      return "\\v";
+    case EOF:
+      return "EOF";
+    default:
+      break;
+  }
+  return NULL;
+}
+
+#endif  // PDCPL_STRING_H_
--- src/1.6.c
+++ src/1.6.c
@@ -5,22 +5,6 @@
  * @copyright MIT License
  */
 
-#ifdef _WIN32
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
-#else
-// need POSIX feature for fmemopen on non-Windows systems
-#define _POSIX_C_SOURCE 200809L
-#endif  // _WIN32
-
-#include <errno.h>
-#ifdef _WIN32
-#include <fcntl.h>
-#include <inttypes.h>
-#include <io.h>
-#include <limits.h>
-#include <stdint.h>
-#endif  // _WIN32
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -28,132 +12,43 @@
 #define PDCPL_HAS_PROGRAM_USAGE
 #include "pdcpl/cliopts.h"
 #include "pdcpl/core.h"
-
-// on warning level 3 or above, MSVC warns that strerror() is unsafe, so we
-// create a wrapper that disables the warning and use strerror() as a macro
-#ifdef _WIN32
-static const char * win32_strerror(int errnum)
-{
-__pragma(warning (push))
-__pragma(warning (disable: 4996))
-  const char* errstr = strerror(errnum);
-__pragma(warning (pop))
-  return errstr;
-}
-
-#define strerror(eno) win32_strerror(eno)
-#endif  // _WIN32
-
-/**
- * Handle an error that results in `errno` being set with an optional message.
- *
- * @param has_error `true` if error, `false` if no error
- * @param message Custom message to use before displaying `errno` message
- */
-#define PDCPL_HANDLE_ERRNO_EX(has_error, message) \
-  do { \
-    if (has_error) { \
-      if (strlen(message)) \
-        fprintf(stderr, "Error: %s: %s\n", message, strerror(errno)); \
-      else \
-        fprintf(stderr, "Error: %s\n", strerror(errno)); \
-      return EXIT_FAILURE; \
-    } \
-  } \
-  while (0)
-
-/**
- * Handle an error that results in `errno` being set.
- *
- * @param has_error `true` if error, `false` if no error
- */
-#define PDCPL_HANDLE_ERRNO(has_error) PDCPL_HANDLE_ERRNO_EX(has_error, "")
-
-#define BUF_CONTENTS "abcdefg"
+#include "pdcpl/string.h"
 
 PDCPL_PROGRAM_USAGE_DEF
 (
-  "Verifies that getchar() != EOF is either 0 or 1."
-  "\n\n"
-  "Checks the expression value for each character in \"" BUF_CONTENTS "\" as\n"
-  "if the contents were piped into stdin on the command line."
-  "\n\n"
-  "For ease of validation, we actually use fgetc() instead of getchar() and\n"
-  "simply read the contents from a FILE * stream. getchar() is typically a\n"
-  "layer over getc(stdin) or fgetc(stdin) anyways."
+  "Verifies that getchar() != EOF is either 0 or 1.\n"
+  "\n"
+  "Reads characters from stdin until EOF is detected and prints the value of\n"
+  "getchar() != EOF for each character, including the final EOF.\n"
+  "\n"
+  "Note that echo appends an extra newline to its output, so if one piped the\n"
+  "output of `echo hello' to this program, one would get output like\n"
+  "\n"
+  "  getchar() != EOF (1) 'h'\n"
+  "  getchar() != EOF (1) 'e'\n"
+  "  getchar() != EOF (1) 'l'\n"
+  "  getchar() != EOF (1) 'l'\n"
+  "  getchar() != EOF (1) 'o'\n"
+  "  getchar() != EOF (1) '\\n'\n"
+  "  getchar() != EOF (0) 'EOF'"
 )
 
 PDCPL_ARG_MAIN
 {
   PDCPL_HANDLE_INFO_OPTS();
-  // string to treat as in-memory file, its length, and its associated stream
-  const char *buf = BUF_CONTENTS;
-// on Windows, buf_len, stream unused (until we figure out what to do)
-#ifdef _MSC_VER
-#pragma warning (push)
-#pragma warning (disable: 4101 4189)
-#endif  // _MSC_VER
-  size_t buf_len = strlen(buf);
-  FILE *stream;
-#ifdef _MSC_VER
-#pragma warning (pop)
-#endif  // _MSC_VER
-#ifdef _WIN32
-  printf("No Windows impementation yet.\n");
-  /*
-  HANDLE buf_handle = CreateFileMappingA(
-    INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, (DWORD) buf_len, NULL
-  );
-  if (!buf_handle) {
-    fprintf(
-      stderr,
-      "Error: Could not create file mapping handle. HRESULT: %" PRIx32 "\n",
-      (int32_t) HRESULT_FROM_WIN32(GetLastError())
-    );
-    return EXIT_FAILURE;
-  }
-  LPVOID buf_map = MapViewOfFile(buf_handle, FILE_MAP_WRITE, 0, 0, buf_len);
-  if (!buf_map) {
-    fprintf(
-      stderr,
-      "Error: Could not create file map view. HRESULT: %" PRIx32 "\n",
-      (int32_t) HRESULT_FROM_WIN32(GetLastError())
-    );
-    if (!CloseHandle(buf_handle))
-      fprintf(
-        stderr,
-        "Error: Could not close file mapping handle. HRESULT: %" PRIx32 "\n",
-        (int32_t) HRESULT_FROM_WIN32(GetLastError())
-      );
-    return EXIT_FAILURE;
-  }
-  CopyMemory(buf_map, buf, buf_len);
-  int buf_fd = _open_osfhandle((intptr_t) buf_handle, _O_RDONLY);
-  if (buf_fd == -1) {
-    fprintf(
-      stderr,
-      "Error: Could not open OS file descriptor from handle. "
-        "HRESULT: %" PRIx32 "\n",
-      (int32_t) HRESULT_FROM_WIN32(GetLastError())
-    );
-    CloseHandle(buf_handle);  // check error
-    UnmapViewOfFile(buf_map);  // check error
-    return EXIT_FAILURE;
-  }
-  stream = _fdopen(buf_fd, "r");
-  */
-#else
-  // none of this is reachable on Windows (yet)
-  stream = fmemopen((void *) buf, buf_len, "r");
-  PDCPL_HANDLE_ERRNO(!stream);
-  // holds the value of pseudo getchar() != EOF
-  int neof;
+  // char value (or EOF), value of pseudo getchar() != EOF
+  int c, neof;
+  // escaped string representation of c if it is an escape char or EOF
+  const char *c_esc;
   do {
-    printf("getchar() /*pseudo*/ != EOF (%d)\n", neof = (fgetc(stream) != EOF));
+    printf("getchar() != EOF (%d) ", neof = ((c = getchar()) != EOF));
+    // MSVC complains about assignment in conditional, so we add an explicit
+    // != NULL. for GCC, the extra parenthese are enough to make it happy.
+    if ((c_esc = pdcpl_stresc(c)) != NULL)
+      printf("'%s'\n", c_esc);
+    else
+      printf("'%c'\n", c);
   }
   while (neof);
-  // close file to clean up
-  PDCPL_HANDLE_ERRNO(fclose(stream));
-#endif  // _WIN32
   return EXIT_SUCCESS;
 }
