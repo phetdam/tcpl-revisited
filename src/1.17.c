319011dc48e27cb1df020c37e67628f46734b992
Derek Huang <djh458@stern.nyu.edu>
2023-02-16 00:00:59 -0500

    add 1.17.c and CMake target

---
+++ src/1.17.c
@@ -0,0 +1,41 @@
+/**
+ * @file 1.17.c
+ * @author Derek Huang
+ * @brief The C Programming Language exercise 1-17
+ * @copyright MIT License
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PDCPL_HAS_PROGRAM_USAGE
+#include "pdcpl/cliopts.h"
+#include "pdcpl/core.h"
+#include "pdcpl/string.h"
+
+PDCPL_PROGRAM_USAGE_DEF
+(
+  "Prints all lines read from stdin longer than 80 chars to stdout\n"
+  "\n"
+  "Lines up to SIZE_MAX - 1 will be correctly printed while any lines that\n"
+  "are SIZE_MAX or longer will trigger appropriate error handling and exit."
+)
+
+PDCPL_ARG_MAIN
+{
+  PDCPL_HANDLE_INFO_OPTS();
+  // pointer to current line buffer and its size
+  char *line;
+  size_t len;
+  // keep going until we reach EOF
+  while (!feof(stdin)) {
+    // read line from stdin, setting buffer address and line length
+    if (pdcpl_getline(stdin, &line, &len))
+      return EXIT_FAILURE;
+    // print if greater than 80 chars in length + free
+    if (len > 80)
+      printf("%s\n", line);
+    free(line);
+  }
+  return EXIT_SUCCESS;
+}
