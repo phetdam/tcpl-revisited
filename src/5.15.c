22683798b92f1252027f4d650a5215746d6d74d8
Derek Huang <djh458@stern.nyu.edu>
2023-03-18 18:36:27 -0400

    5.15.c: prepare for inclusio of exercise 5-16 directory order
    Adds enum to control sorting mode, updates actions and adds option
    and action for directory sort (doesn't work with -f currently), rename
    strptr_numcmp[_r] tp strptr_ncmp[_r] to shorten name, fix compare
    function docs (some have wrong comparison order), add strptr_dcmp[_r]
    for directory order ascending/descending comparison, update set_qsort_cmp
    to take sort mode instead of multiple booleans

--- src/5.15.c
+++ src/5.15.c
@@ -32,11 +32,24 @@
 #define PDCPL_SORT_CHUNK_LINES 1024
 
 /**
+ * Enum for the possible sorting methods.
+ *
+ * `SORT_MODE_MAX` just gives the total number of sort modes available.
+ */
+typedef enum {
+  SORT_MODE_DEFAULT,
+  SORT_MODE_DEFAULT_IGNORE_CASE,
+  SORT_MODE_NUMERIC,
+  SORT_MODE_DIRECTORY,
+  SORT_MODE_DIRECTORY_IGNORE_CASE,
+  SORT_MODE_MAX
+} sort_mode;
+
+/**
  * Static globals set during program option parsing.
  */
-static bool numeric_sort_target = false;
+static sort_mode sort_program_mode = SORT_MODE_DEFAULT;
 static bool reverse_target = false;
-static bool ignore_case_target = false;
 static size_t chunk_lines_target = PDCPL_SORT_CHUNK_LINES;
 
 /**
@@ -46,7 +59,18 @@ static
 PDCPL_CLIOPT_ACTION(numeric_sort_action)
 {
   PDCPL_CLIOPT_ACTION_NO_WARN_UNUSED
-  numeric_sort_target = true;
+  sort_program_mode = SORT_MODE_NUMERIC;
+  return PDCPL_CLIOPT_PARSE_OK;
+}
+
+/**
+ * Action to determine wheter or not we sort using directory order.
+ */
+static
+PDCPL_CLIOPT_ACTION(directory_sort_action)
+{
+  PDCPL_CLIOPT_ACTION_NO_WARN_UNUSED
+  sort_program_mode = SORT_MODE_DIRECTORY;
   return PDCPL_CLIOPT_PARSE_OK;
 }
 
@@ -68,7 +92,13 @@ static
 PDCPL_CLIOPT_ACTION(ignore_case_action)
 {
   PDCPL_CLIOPT_ACTION_NO_WARN_UNUSED
-  ignore_case_target = true;
+  switch (sort_program_mode) {
+    case SORT_MODE_DEFAULT:
+      sort_program_mode = SORT_MODE_DEFAULT_IGNORE_CASE;
+      break;
+    default:
+      break;
+  }
   return PDCPL_CLIOPT_PARSE_OK;
 }
 
@@ -111,6 +141,15 @@ PDCPL_PROGRAM_OPTIONS_DEF
     NULL
   },
   {
+    "-d", "--directory-sort",
+    "Compare lines only by their alphanumeric or whitespace characters. If "
+    "specified with -n, --numeric-sort, the last specified option will be "
+    "chosen as the sorting method.",
+    0,
+    directory_sort_action,
+    NULL
+  },
+  {
     "-r", "--reverse",
     "Sort in descending instead of ascending order",
     0,
@@ -120,7 +159,7 @@ PDCPL_PROGRAM_OPTIONS_DEF
   {
     "-f", "--ignore-case",
     "Ignore alphabetic case when sorting. Has no effect if specified with -n, "
-    "--numeric-sort",
+    "--numeric-sort or with -d, --directory-sort",
     0,
     ignore_case_action,
     NULL
@@ -263,11 +302,11 @@ strptr_lcmp_r(const char **a, const char **b)
  *
  * @param a Address of first string
  * @param b Address of second string
- * @returns -1 if `atof(*b)` is less than `atof(*a)`, 0 if the `atof` values
- *  are equal, 1 when `atof(*a)` is greater than `atof(*b)`
+ * @returns -1 if `atof(*a)` is less than `atof(*b)`, 0 if the `atof` values
+ *  are equal, 1 when `atof(*b)` is less than `atof(*a)`
  */
 static int
-strptr_numcmp(const char **a, const char **b)
+strptr_ncmp(const char **a, const char **b)
 {
   double va = atof(*a);
   double vb = atof(*b);
@@ -283,42 +322,90 @@ strptr_numcmp(const char **a, const char **b)
  *
  * @param a Address of first string
  * @param b Address of second string
- * @returns -1 if `atof(*b)` is greater than `atof(*a)`, 0 if the `atof` values
+ * @returns -1 if `atof(*b)` is less than `atof(*a)`, 0 if the `atof` values
  *  are equal, 1 when `atof(*a)` is less than `atof(*b)`
  */
 static inline int
-strptr_numcmp_r(const char **a, const char **b)
+strptr_ncmp_r(const char **a, const char **b)
 {
-  return -strptr_numcmp(a, b);
+  return -strptr_ncmp(a, b);
+}
+
+/**
+ * Compare two strings by directory order.
+ *
+ * @param a Address of first string
+ * @param b Address of second string
+ * @returns -1 if `*a` has directory order before `*b`, 0 if equal in terms of
+ *  directory order, 1 when `*b` has directory order before `*a`
+ */
+static int
+strptr_dcmp(const char **a, const char **b)
+{
+  // strings and current char of strings
+  const char *sa, *sb;
+  char ca, cb;
+  // compare only on alphanumeric or whitespace chars until we break
+  for (sa = *a, sb = *b; (ca = *sa) != '\0' && (cb = *sb) != '\0'; sa++, sb++) {
+    // advance sa, sb until *sa, *sb are both whitespace/alphanumeric
+    if (ca != '\0' && ca != ' ' && !isalnum(ca))
+      ca = *sa++;
+    if (cb != '\0' && ca != ' ' && !isalnum(cb))
+      cb = *sb++;
+    // compare. this is still correct even if ca, cb are '\0'
+    if (ca < cb)
+      return -1;
+    if (cb < ca)
+      return 1;
+  }
+  // if we didn't break in the loop, then the strings are equal
+  return 0;
+}
+
+/**
+ * Compare two strings in reverse by directory order.
+ *
+ * @param a Address of first string
+ * @param b Address of second string
+ * @returns -1 if `*b` has directory order before `*a`, 0 if equal in terms of
+ *  directory order, 1 when `*a` has directory order before `*b`
+ */
+static inline int
+strptr_dcmp_r(const char **a, const char **b)
+{
+  return -strptr_dcmp(a, b);
 }
 
 /**
  * Determine which `qsort` compare function should be used.
  *
  * @param out Address to a `qsort_cmp` to use as the compare function
- * @param num_sort `true` to sort by `atof` value, `false` not to
+ * @param mode Program sort mode
  * @param rev_sort `true` to sort in reverse, `false` not to
- * @param ignore_case `true` to ignore case when sorting, `false` not to
  * @returns 0 on success, -EINVAL if `out` is `NULL`
  */
 static int
-set_qsort_cmp(qsort_cmp *out, bool num_sort, bool rev_sort, bool ignore_case)
+set_qsort_cmp(qsort_cmp *out, sort_mode mode, bool rev_sort)
 {
   if (!out)
     return -EINVAL;
-  qsort_cmp cmp;
-  // if we are using numeric sort
-  if (num_sort)
-    cmp = (rev_sort) ? (qsort_cmp) strptr_numcmp_r : (qsort_cmp) strptr_numcmp;
-  // otherwise case-insensitive string-based sorting
-  else if (ignore_case) {
-    cmp = (rev_sort) ? (qsort_cmp) strptr_lcmp_r : (qsort_cmp) strptr_lcmp;
+  switch (mode) {
+    case SORT_MODE_DEFAULT:
+      *out = (rev_sort) ? (qsort_cmp) strptr_cmp_r : (qsort_cmp) strptr_cmp;
+      break;
+    case SORT_MODE_DEFAULT_IGNORE_CASE:
+      *out = (rev_sort) ? (qsort_cmp) strptr_lcmp_r : (qsort_cmp) strptr_lcmp;
+      break;
+    case SORT_MODE_NUMERIC:
+      *out = (rev_sort) ? (qsort_cmp) strptr_ncmp_r : (qsort_cmp) strptr_ncmp;
+      break;
+    case SORT_MODE_DIRECTORY:
+      *out = (rev_sort) ? (qsort_cmp) strptr_dcmp_r : (qsort_cmp) strptr_dcmp;
+      break;
+    // case SORT_MODE_DIRECTORY_IGNORE_CASE:
+    default:
+      return -EINVAL;
   }
-  // otherwise standard string-based sorting
-  else
-    cmp = (rev_sort) ? (qsort_cmp) strptr_cmp_r : (qsort_cmp) strptr_cmp;
-  // finally done
-  *out = cmp;
   return 0;
 }
 
@@ -346,7 +433,7 @@ PDCPL_ARG_MAIN
   ERRNO_EXIT(pdcpl_buffer_realloc(&linebuf, n_lines * sizeof(char **)));
   // set compare function according to options
   qsort_cmp cmp;
-  set_qsort_cmp(&cmp, numeric_sort_target, reverse_target, ignore_case_target);
+  set_qsort_cmp(&cmp, sort_program_mode, reverse_target);
   qsort(linebuf.data, n_lines, sizeof(char **), cmp);
   // print out + free the lines stored in linebuf's data pointer
   for (size_t i = 0; i < n_lines; i++) {
