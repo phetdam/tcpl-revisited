38490a633764df5ce96d309dc7905b597cf6ab1d
Derek Huang <djh458@stern.nyu.edu>
2023-07-13 01:46:57 -0400

    string_param_test.cc: refactor to remove StringTest from test names, use StringTest in param suites

--- test/string_param_test.cc
+++ test/string_param_test.cc
@@ -21,11 +21,11 @@ namespace {
 /**
  * Test fixture for parametrized testing of `pdcpl_print[p]wtd`.
  */
-class PrintWidthStringTest : public ::testing::TestWithParam<
+class PrintWidthTest : public ::testing::TestWithParam<
   std::tuple<std::ptrdiff_t, unsigned short, unsigned short>> {
 public:
   /**
-   * Return an input for `PrintWidthStringTest` parametrized tests.
+   * Return an input for `PrintWidthTest` parametrized tests.
    *
    * @param value Input value
    * @param padding Print width padding
@@ -45,7 +45,7 @@ public:
 /**
  * Test that `pdcpl_printpwtd` and `pdcpl_printwtd` work as expected.
  */
-TEST_P(PrintWidthStringTest, ParamTest)
+TEST_P(PrintWidthTest, ParamTest)
 {
   // input, padding, padded print width
   const auto value = std::get<0>(GetParam());
@@ -57,25 +57,25 @@ TEST_P(PrintWidthStringTest, ParamTest)
 }
 
 INSTANTIATE_TEST_SUITE_P(
-  PrintWidthParams,
-  PrintWidthStringTest,
+  StringTest,
+  PrintWidthTest,
   ::testing::Values(
-    PrintWidthStringTest::CreateInput(123513272, 2),
-    PrintWidthStringTest::CreateInput(-1991823, 3),
-    PrintWidthStringTest::CreateInput(8787822, 6)
+    PrintWidthTest::CreateInput(123513272, 2),
+    PrintWidthTest::CreateInput(-1991823, 3),
+    PrintWidthTest::CreateInput(8787822, 6)
   )
 );
 
 /**
  * Test fixture for parametrized testing of `pdcpl_next_tab_(stop|size)`.
  */
-class TabStopStringTest : public ::testing::TestWithParam<
+class TabStopTest : public ::testing::TestWithParam<
   std::tuple<std::size_t, unsigned int, std::size_t>> {};
 
 /**
  * Test that `pdcpl_next_tab_(stop|size)` work as expected.
  */
-TEST_P(TabStopStringTest, ParamTest)
+TEST_P(TabStopTest, ParamTest)
 {
   // current column position + tab size + next expected tab stop
   const auto cur_col = std::get<0>(GetParam());
@@ -87,26 +87,26 @@ TEST_P(TabStopStringTest, ParamTest)
 }
 
 INSTANTIATE_TEST_SUITE_P(
-  TabStopParams,
-  TabStopStringTest,
+  StringTest,
+  TabStopTest,
   ::testing::Values(
-    TabStopStringTest::ParamType{17, 5, 20},
-    TabStopStringTest::ParamType{30, 7, 35},
-    TabStopStringTest::ParamType{15, 4, 16},
-    TabStopStringTest::ParamType{20, 3, 21}
+    TabStopTest::ParamType{17, 5, 20},
+    TabStopTest::ParamType{30, 7, 35},
+    TabStopTest::ParamType{15, 4, 16},
+    TabStopTest::ParamType{20, 3, 21}
   )
 );
 
 /**
  * Test fixture for parametrized testing of `pdcpl_htoj`.
  */
-class HexConvertStringTest : public ::testing::TestWithParam<
+class HexConvertTest : public ::testing::TestWithParam<
   std::pair<std::string, std::intmax_t>> {};
 
 /**
  * Test that `pdcpl_htoj` works as expected.
  */
-TEST_P(HexConvertStringTest, ParamTest)
+TEST_P(HexConvertTest, ParamTest)
 {
   // target intmax_t value to convert hex string to
   std::intmax_t value;
@@ -115,26 +115,26 @@ TEST_P(HexConvertStringTest, ParamTest)
 }
 
 INSTANTIATE_TEST_SUITE_P(
-  HexConvertParams,
-  HexConvertStringTest,
+  StringTest,
+  HexConvertTest,
   ::testing::Values(
-    HexConvertStringTest::ParamType{"0xdeadbeef", 0xdeadbeef},
-    HexConvertStringTest::ParamType{"-0xAFE12", -0xafe12},
-    HexConvertStringTest::ParamType{"-0XaE098FbD0", -0xae098fbd0},
-    HexConvertStringTest::ParamType{"34343dae", 0x34343dae}
+    HexConvertTest::ParamType{"0xdeadbeef", 0xdeadbeef},
+    HexConvertTest::ParamType{"-0xAFE12", -0xafe12},
+    HexConvertTest::ParamType{"-0XaE098FbD0", -0xae098fbd0},
+    HexConvertTest::ParamType{"34343dae", 0x34343dae}
   )
 );
 
 /**
  * Test fixture for parametrized testing of `pdcpl_strsq`.
  */
-class SqueezeStringTest : public ::testing::TestWithParam<
+class SqueezeTest : public ::testing::TestWithParam<
   std::tuple<std::string, std::string, std::string>> {};
 
 /**
  * Test that `pdcpl_strsq` works as expected.
  */
-TEST_P(SqueezeStringTest, ParamTest)
+TEST_P(SqueezeTest, ParamTest)
 {
   // must be freed after every successful call
   char *res;
@@ -149,12 +149,12 @@ TEST_P(SqueezeStringTest, ParamTest)
 }
 
 INSTANTIATE_TEST_SUITE_P(
-  SqueezeParams,
-  SqueezeStringTest,
+  StringTest,
+  SqueezeTest,
   ::testing::Values(
-    SqueezeStringTest::ParamType{"totally", "holy", "tta"},
-    SqueezeStringTest::ParamType{"normally", "", "normally"},
-    SqueezeStringTest::ParamType{"Veronica", "acinoVer", ""}
+    SqueezeTest::ParamType{"totally", "holy", "tta"},
+    SqueezeTest::ParamType{"normally", "", "normally"},
+    SqueezeTest::ParamType{"Veronica", "acinoVer", ""}
   )
 );
 
