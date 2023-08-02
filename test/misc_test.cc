e2f80b2a05c2ed57e7ac933beb4618ecfdce39bc
Derek Huang <djh458@stern.nyu.edu>
2023-08-02 02:58:18 -0400

    misc_test.cc: removed shared members from class and use local constexpr double instead

--- test/misc_test.cc
+++ test/misc_test.cc
@@ -14,21 +14,19 @@ namespace {
 /**
  * Misc functions test fixture class.
  */
-class MiscTest : public ::testing::Test {
-protected:
-  // freezing point in Celsius and Fahrenheit
-  static inline constexpr double freezing_c_ = 0;
-  static inline constexpr double freezing_f_ = 32;
-};
+class MiscTest : public ::testing::Test {};
 
 /**
  * Test that Fahrenheit to Celsius conversion and vice versa work as expected.
  */
 TEST_F(MiscTest, TempTest)
 {
+  // freezing point in Celsius and Fahrenheit
+  constexpr double freezing_c = 0;
+  constexpr double freezing_f = 32;
   // 0 C* is same as 32 F*
-  EXPECT_DOUBLE_EQ(freezing_c_, pdcpl_f2ctemp(freezing_f_));
-  EXPECT_DOUBLE_EQ(freezing_f_, pdcpl_c2ftemp(freezing_c_));
+  EXPECT_DOUBLE_EQ(freezing_c, pdcpl_f2ctemp(freezing_f));
+  EXPECT_DOUBLE_EQ(freezing_f, pdcpl_c2ftemp(freezing_c));
 }
 
 }  // namespace
