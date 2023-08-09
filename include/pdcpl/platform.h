5708d0452b8f32ec4bf2c17def8ae01476afe5db
Derek Huang <djh458@stern.nyu.edu>
2023-08-09 01:51:19 -0400

    platform.h: update macro comments

--- include/pdcpl/platform.h
+++ include/pdcpl/platform.h
@@ -26,7 +26,8 @@
 #define PDCPL_SYSTEM_NAME "Linux"
 #else
 #define PDCPL_SYSTEM_NAME "unknown"
-#endif
+#endif  // !defined(_WIN32) && (!defined(__APPLE__) || !defined(__MACH__)) &&
+        // !defined(__CYGWIN__) && !defined(__linux__)
 #endif  // PDCPL_SYSTEM_NAME
 
 // system version, hard to determine. real build uses CMAKE_SYSTEM_VERSION
@@ -37,16 +38,16 @@
 // system arch, hard to determine. real build uses CMAKE_SYSTEM_PROCESSOR
 #ifndef PDCPL_SYSTEM_ARCH
 #if defined(_WIN32)
-#ifdef _WIN64
+#if defined(_WIN64)
 #define PDCPL_SYSTEM_ARCH "AMD64"
 #else
 #define PDCPL_SYSTEM_ARCH "x86"
-#endif  // !_WIN64
+#endif  // !defined(_WIN64)
 #elif defined(__linux__)
 #define PDCPL_SYSTEM_ARCH "x86_64"
 #else
 #define PDCPL_SYSTEM_ARCH "unknown"
-#endif
+#endif  // !defined(_WIN32) && !defined(__linux__)
 #endif  // PDCPL_SYSTEM_ARCH
 
 #endif  // PDCPL_PLATFORM_H_
